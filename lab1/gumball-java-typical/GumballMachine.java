9aae745e5fa3570c8ffb745c8296a340fe1f6bb7
tapankulkarni-sjsu <takulkar@cisco.com>
2018-09-08 14:28:21 -0700

    changes for lab

--- lab1/gumball-java-typical/GumballMachine.java
+++ lab1/gumball-java-typical/GumballMachine.java
@@ -1,33 +1,40 @@
+import java.util.Set;
 
 public class GumballMachine
 {
 
     private int num_gumballs;
-    private boolean has_quarter;
+    private int cost;
+    private Set<Integer> acceptableCoins;
+    private int totalInsertedValue;
 
-    public GumballMachine( int size )
+    public GumballMachine( int size , int cost, Set<Integer> acceptableCoins)
     {
         // initialise instance variables
         this.num_gumballs = size;
-        this.has_quarter = false;
+        this.cost = cost;
+        this.acceptableCoins = acceptableCoins;
     }
 
-    public void insertQuarter(int coin)
+    public void insertCoin(int coin)
     {
-        if ( coin == 25 )
-            this.has_quarter = true ;
-        else 
-            this.has_quarter = false ;
+    	System.out.println("Coin inserted of value : "+ coin);
+        if ( acceptableCoins.contains(coin) ){
+        	this.totalInsertedValue += coin;
+        }else{ //No way to eject coins
+            System.out.println("Please insert a coin of one following values :" + acceptableCoins.toString());
+    	}
     }
     
     public void turnCrank()
     {
-    	if ( this.has_quarter )
+    	System.out.println("Crank turned!!!");
+    	if ( isPurchasable() )
     	{
     		if ( this.num_gumballs > 0 )
     		{
     			this.num_gumballs-- ;
-    			this.has_quarter = false ;
+    			this.totalInsertedValue = 0;
     			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
     		}
     		else
@@ -37,7 +44,18 @@ public class GumballMachine
     	}
     	else 
     	{
-    		System.out.println( "Please insert a quarter" ) ;
+    		System.out.println("Cost of a gumball is : "+ cost + ". Please inset more coins with total value : "+ (this.cost-this.totalInsertedValue));
     	}        
     }
+
+	private boolean isPurchasable() {
+		return this.totalInsertedValue >= cost; // No way to eject coins. So additional coins will be used for single gumball. 
+	}
+
+	@Override
+	public String toString() {
+		return "GumballMachine [num_gumballs=" + num_gumballs + ", cost=" + cost + ", acceptableCoins="
+				+ acceptableCoins + ", totalInsertedValue=" + totalInsertedValue + "]";
+	}
+	
 }
--- lab1/gumball-java-typical/Main.java
+++ lab1/gumball-java-typical/Main.java
@@ -1,22 +1,86 @@
-
+import java.util.TreeSet;
+import java.util.Set;
 
 public class Main {
 
 	public static void main(String[] args) {
-		GumballMachine gumballMachine = new GumballMachine(5);
+		executeGumballMachineCase1();
+		executeGumballMachineCase2();
+		executeGumballMachineCase3();
+	}
 
-		System.out.println(gumballMachine);
+	private static void executeGumballMachineCase1() {
+		System.out.println("----Case 1 Start----");
+		Set<Integer> acceptableCoins = new TreeSet<>();
+		acceptableCoins.add(25);
+		GumballMachine gumballMachineCase1 = new GumballMachine(5, 25, acceptableCoins );
 
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
+		System.out.println(gumballMachineCase1);
 
-		System.out.println(gumballMachine);
+		gumballMachineCase1.insertCoin( 25 );
+		gumballMachineCase1.turnCrank();
 
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
+		System.out.println(gumballMachineCase1);
 
-		System.out.println(gumballMachine);
+		gumballMachineCase1.insertCoin( 25 );
+		gumballMachineCase1.turnCrank();
+		gumballMachineCase1.insertCoin( 25 );
+		gumballMachineCase1.turnCrank();
+		gumballMachineCase1.insertCoin( 20 );
+		gumballMachineCase1.turnCrank();
+
+		System.out.println(gumballMachineCase1);
+		System.out.println("----Case 1 End----");
+	}
+	
+	private static void executeGumballMachineCase2() {
+		System.out.println("----Case 2 Start----");
+		Set<Integer> acceptableCoins = new TreeSet<>();
+		acceptableCoins.add(25);
+		GumballMachine gumballMachineCase1 = new GumballMachine(5, 50, acceptableCoins );
+
+		System.out.println(gumballMachineCase1);
+
+		gumballMachineCase1.insertCoin( 25 );
+		gumballMachineCase1.turnCrank();
+
+		System.out.println(gumballMachineCase1);
+
+		gumballMachineCase1.insertCoin( 20 );
+		gumballMachineCase1.turnCrank();
+		gumballMachineCase1.insertCoin( 25 );
+		gumballMachineCase1.turnCrank();
+		gumballMachineCase1.insertCoin( 50 );
+		gumballMachineCase1.turnCrank();
+
+		System.out.println(gumballMachineCase1);
+		System.out.println("----Case 2 End----");
+	}
+	
+	private static void executeGumballMachineCase3() {
+		System.out.println("----Case 3 Start----");
+		Set<Integer> acceptableCoins = new TreeSet<>();
+		acceptableCoins.add(25);
+		acceptableCoins.add(10);
+		acceptableCoins.add(5);
+		GumballMachine gumballMachineCase1 = new GumballMachine(5, 50, acceptableCoins );
+
+		System.out.println(gumballMachineCase1);
+
+		gumballMachineCase1.insertCoin( 25 );
+		gumballMachineCase1.turnCrank();
+
+		System.out.println(gumballMachineCase1);
+
+		gumballMachineCase1.insertCoin( 10 );
+		gumballMachineCase1.turnCrank();
+		gumballMachineCase1.insertCoin( 5 );
+		gumballMachineCase1.insertCoin( 10 );
+		gumballMachineCase1.turnCrank();
+		gumballMachineCase1.insertCoin( 50 );
+		gumballMachineCase1.turnCrank();
+
+		System.out.println(gumballMachineCase1);
+		System.out.println("----Case 3 End----");
 	}
 }
